bb5246aae802cfdbc1fed24274090473eea561e7
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-07-16 13:38:32 -0500

    new experiment

--- src/config.py
+++ src/config.py
@@ -3,20 +3,20 @@ config = {
     'initial seed': 1, 
     'device': 'mps', 
     # Data parameters
-    'batch size': 16, 
-    'num workers': 6, 
+    'batch size': 32, 
+    'num workers': 8, 
     'image shape': (1, 28, 28),
     'train samples': 16000, 
     'test samples': 4000,
     # Model parameters
-    'generator blocks' : 3,
-    'discriminator blocks' : 3,
+    'generator blocks' : 2,
+    'discriminator blocks' : 2,
     # Training parameters
     'latent dimension': 64,
     'learning rate': 0.01, 
     'beta1': 0.5,
     'beta2': 0.999,
-    'epochs': 10, 
+    'epochs': 50, 
     'discriminator epochs': 1,
     # Save parameters
     'sample interval': 1,
